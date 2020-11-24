fe808807ac364bec180b5a916c8a09b4973825af
NicolePate <nicole.pate512@gmail.com>
2020-11-23 21:37:39 -0600

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,52 @@
+/*let js = 'amazing';
+console.log(40+8+23-10);
+
+console.log('Jonas');
+console.log(23);
+
+
+let firstName = "matilda";
+
+let myFirstJob = "Programmer";
+let myCurrentJob = 'Teacher';
+
+
+// This is not discriptive **Do Not Do This**
+// let job1 = "Programmer";
+// let job2 = 'Teacher';
+
+console.log(myFirstJob)
+
+7 primitive data types
+1)Number 0
+2)String ""
+3)Boolean true/false
+4)undefined: empty value
+5)also means empty value
+6)Symbol (ES2015)
+7)Biglnt (ES2020) Larger intergers
+*/
+
+
+// LET declares a NEW Value
+let javascriptIsFun = true;
+console.log(true);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "jonas");
+
+// but not usine LET re-defining a existing Value
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year)
+
+year = 1991;
+console.log(year);
+console.log(typeof year)
+
+console.log(typeof null)
